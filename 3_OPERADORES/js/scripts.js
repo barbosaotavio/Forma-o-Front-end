6a5c2707a2dde3c26fbc6fd032d38f6c64c76725
otavio <barbosaotavio293@gmail.com>
2025-05-14 02:24:09 -0300

    teste

--- 3_OPERADORES/js/scripts.js
+++ 3_OPERADORES/js/scripts.js
@@ -3,10 +3,16 @@ console.log(typeof 258)
 console.log(typeof 25.6)
 console.log(typeof 138)
 
-// 2 - operações aritiméticas
+// 2 - Operações aritiméticas
 
 console.log(2+5) // adição
 console.log(15-5) // subtração
 console.log(15*3) // multiplicação
 console.log(20/4) // divisão
 console.log(5+(4*2)) //multiplicação com adição
+
+// 3 - Especial numbers
+
+console.log(typeof Infinity)
+console.log( typeof -Infinity)
+console.log(5/ "teste")
